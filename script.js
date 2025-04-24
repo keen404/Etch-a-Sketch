a38f075afbf080d15c7fae545ef767b6ac47ee5d
Keen <2003keen@gmail.com>
2025-04-24 09:30:39 +0700

    Created a container

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+    // Get grid size assigned to a variable
+    // Loop as grid size
+        // Loop as grid size
+            // Create a div element
+            // Give it class name
+            // Append to div container
+});
