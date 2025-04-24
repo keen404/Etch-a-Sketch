d1c44f8d0900f0af074a6ffb606d1732dde45b25
Keen <2003keen@gmail.com>
2025-04-24 09:52:04 +0700

    Create a grid row to contain the div

--- script.js
+++ script.js
@@ -2,10 +2,13 @@ document.addEventListener("DOMContentLoaded", () => {
     const gridSize = +prompt("Grid size?");
     const container = document.querySelector("#container");
     for (let i = 0; i < gridSize; i++) {
+        const gridRow = document.createElement("div");
+        gridRow.classList.add("row");
+        container.appendChild(gridRow);
         for (let j = 0; j < gridSize; j++) {
             const newDiv = document.createElement("div");
             newDiv.classList.add("aSqure");
-            container.appendChild(newDiv);
+            gridRow.appendChild(newDiv);
         }
     }
 });
