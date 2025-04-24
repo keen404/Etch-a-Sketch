7dec81a5bce5d9fe0f917e158167a2d3c18d6ecb
Keen <2003keen@gmail.com>
2025-04-24 10:42:23 +0700

    Add a button to container

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const gridSize = +prompt("Grid size?");
     const container = document.querySelector("#container");
-    for (let i = 0; i < gridSize; i++) {
+
+    const btn = document.createElement("button");
+    // Add click event to button
+        // Delete all the div inside container
+        // Prompt user for grid size
+        // Assign number to variable
+        // Loop as grid size
+            // Create a row div
+            // Add class name "row"
+            // Appended Child to container
+            // Loop as grid size
+                // Create a div
+                // Add class name "aSqure"
+                // Add event mosuseenter
+                    // Changing "aSqure" background color to black
+    container.appendChild(btn);
+    
+    for (let i = 0; i < 16; i++) {
         const gridRow = document.createElement("div");
         gridRow.classList.add("row");
         container.appendChild(gridRow);
-        for (let j = 0; j < gridSize; j++) {
+        for (let j = 0; j < 16; j++) {
             const newDiv = document.createElement("div");
             newDiv.classList.add("aSqure");
             newDiv.addEventListener("mouseenter", () => {
