2efa0f8519232007b9b6306e89753e9a92e7e68e
Keen <2003keen@gmail.com>
2025-04-24 09:34:02 +0700

    Asking for grid size js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
     // Get grid size assigned to a variable
+    const gridSize = +prompt("Grid size?");
     // Loop as grid size
         // Loop as grid size
             // Create a div element
